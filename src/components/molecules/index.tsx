84560d46298184206ab6a2f5bdb730ca140a8b16
MrPapa-Ux <tosans977@gmail.com>
2024-10-30 14:19:24 +0800

    transactioncard

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -1,5 +1,6 @@
 import Header from './header';
 import TextInput from './textinput';
 import List from './list';
+import TransactionCard from './transactioncard';
 
-export {Header, TextInput, List};
+export {Header, TextInput, List, TransactionCard};
