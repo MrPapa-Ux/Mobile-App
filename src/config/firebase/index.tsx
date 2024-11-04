1a779b238176dac7370d3bc82832b2a5e0b95766
MrPapa-Ux <tosans977@gmail.com>
2024-11-04 20:59:55 +0800

    tes for class

---
+++ src/config/firebase/index.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVEZ3XXWM7jBozA4RBtl7yHiOAn9YBK_M',
+  authDomain: 'chipzmoneytracker-mad.firebaseapp.com',
+  projectId: 'chipzmoneytracker-mad',
+  storageBucket: 'chipzmoneytracker-mad.firebasestorage.app',
+  messagingSenderId: '802230772563',
+  appId: '1:802230772563:web:3bebed2636874fb36037a6',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
