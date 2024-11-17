8f6c56f2ae916e826e6b0c6ac2c328baa704bcbb
MrPapa-Ux <tosans977@gmail.com>
2024-11-18 07:20:15 +0800

    almost done

--- App.tsx
+++ App.tsx
@@ -1,58 +1,46 @@
 import React from 'react';
 import SplashScreen from './src/pages/SplashScreen';
-import {NavigationContainer} from '@react-navigation/native';
 import SignIn from './src/pages/SignIn';
 import SignUp from './src/pages/SignUp';
 import Home from './src/pages/Home';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AddTransaction from './src/pages/AddTransaction';
 import FlashMessage from 'react-native-flash-message';
-import CashOnHand from './src/pages/CashOnHand';
-import CashOnBank from './src/pages/CashOnBank';
 import './src/config/Firebase';
 
-const stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <stack.Navigator>
-        <stack.Screen
+      <Stack.Navigator>
+        <Stack.Screen
           name="SplashScreen"
           component={SplashScreen}
           options={{headerShown: false}}
         />
-        <stack.Screen
+        <Stack.Screen
           name="SignIn"
           component={SignIn}
           options={{headerShown: false}}
         />
-        <stack.Screen
+        <Stack.Screen
           name="SignUp"
           component={SignUp}
           options={{headerShown: false}}
         />
-        <stack.Screen
+        <Stack.Screen
           name="Home"
           component={Home}
           options={{headerShown: false}}
         />
-        <stack.Screen
+        <Stack.Screen
           name="AddTransaction"
           component={AddTransaction}
           options={{headerShown: false}}
         />
-        <stack.Screen
-          name="CashOnHand"
-          component={CashOnHand}
-          options={{headerShown: false}}
-        />
-        <stack.Screen
-          name="CashOnBank"
-          component={CashOnBank}
-          options={{headerShown: false}}
-        />
-      </stack.Navigator>
+      </Stack.Navigator>
       <FlashMessage position="top" />
     </NavigationContainer>
   );
